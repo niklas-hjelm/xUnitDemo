e2bba1f2d82049fbc29253f405ed3b9c00f65897
niklas-hjelm <niklas.hjelm@iths.se>
2023-11-17 13:25:14 +0100

    Add project files.

---
+++ xUnitDemo/Calculator.cs
@@ -0,0 +1,24 @@
+﻿namespace xUnitDemo;
+
+public class Calculator : ICalculator
+{
+    public decimal Add(decimal a, decimal b)
+    {
+        return a+b;
+    }
+
+    public decimal Subtract(decimal a, decimal b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Multiply(decimal a, decimal b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Divide(decimal a, decimal b)
+    {
+        throw new NotImplementedException();
+    }
+}
